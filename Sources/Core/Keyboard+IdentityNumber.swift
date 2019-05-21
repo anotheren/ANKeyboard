57965844a1c384020bbd3992b632fd5a4c601717
liudong <liudong.edward@gmail.com>
2019-05-21 11:39:37 +0800

    修复命名

--- Sources/Core/Keyboard+IdentityNumber.swift
+++ Sources/Core/Keyboard+IdentityNumber.swift
@@ -8,18 +8,18 @@
 
 import Foundation
 
-fileprivate let identityNumberMaxLength = 18
+private let identityCardNumberMaxLength = 18
 
 extension Keyboard: IdentityCardNumberKeyboardDelegate {
     
     func keyboard(_ keyboard: IdentityCardNumberKeyboard, didEnter key: String) {
         switch bindType {
         case .textFiled:
-            if let text = textField?.text, text.count < identityNumberMaxLength {
+            if let text = textField?.text, text.count < identityCardNumberMaxLength {
                 textField?.insertText(key)
             }
         case .textView:
-            if let text = textView?.text, text.count < identityNumberMaxLength {
+            if let text = textView?.text, text.count < identityCardNumberMaxLength {
                 textView?.insertText(key)
             }
         case .none:
